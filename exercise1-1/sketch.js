98533ad2ad88fa0ee2dbb0271c5c56800694c06b
tcc545-art <tcc545@york.ac.uk>
2025-11-10 13:19:51 +0000

    43

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(600,600);
+}
+
+function draw(){
+    background(255);
+
+    fill(0);
+    textAlign(RIGHT,BOTTOM);
+    textSize(20);
+    text("Lorem ipsum dolor sit amet",300,300);
+
+}
+
+
+//Lorem ipsum dolor sit amet
